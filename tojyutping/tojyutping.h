c38ca2d63526a80033ea381fc35162b43245cac5
Devon Crebbin <devon@land.org.au>
2024-12-09 17:52:30 +1030

    Initial Commit

---
+++ tojyutping/tojyutping.h
@@ -0,0 +1,18 @@
+//
+//  tojyutping.h
+//  tojyutping
+//
+//  Created by Devon Crebbin on 9/12/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for tojyutping.
+FOUNDATION_EXPORT double tojyutpingVersionNumber;
+
+//! Project version string for tojyutping.
+FOUNDATION_EXPORT const unsigned char tojyutpingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tojyutping/PublicHeader.h>
+
+
---
+++ tojyutpingTests/tojyutpingTests.swift
@@ -0,0 +1,17 @@
+//
+//  tojyutpingTests.swift
+//  tojyutpingTests
+//
+//  Created by Devon Crebbin on 9/12/2024.
+//
+
+import Testing
+@testable import tojyutping
+
+struct tojyutpingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
